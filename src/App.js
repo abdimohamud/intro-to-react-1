17a907b22d7bd8e357937e4d3f800e7da2a07894
Abdi <abdinajibmohamud@gmail.com>
2021-05-20 05:18:56 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Counter from './components/Counter';
+import Input from './components/Input';
+import Moods from './components/Moods';
+import Spinner from './components/Spinner';
+import Squares from './components/Squares';
+import Programmers from './components/Programmers';
+
+function App() {
+  return (
+    <div>
+    <Counter />
+    <Moods />
+    <Spinner />
+    <Input />
+    <Squares />
+    <Programmers />
+  </div>
+  );
+}
+
+export default App;
